e6f694192c5019b284d4e3669048bf9ca7049599
Daniel Muñoz <dmunoz@uloyola.es>
2023-11-22 19:07:54 +0100

    first_commit

---
+++ BJ_framework.py
@@ -0,0 +1,183 @@
+import tkinter as tk
+from tkinter import PhotoImage, messagebox
+import random
+
+class Card:
+    def __init__(self, suit: str, value: int):
+        # TODO: Initialize the attributes
+        pass
+
+    def get_numeric_value(self) -> int:
+        # TODO: Return the numeric value of the card
+        pass
+
+    def get_image(self):
+        # TODO: Return the path to the card's image
+        pass
+
+class Deck:
+    def __init__(self):
+        # TODO: Initialize the deck
+        pass
+
+    def shuffle(self):
+        # TODO: Shuffle the cards
+        pass
+
+    def deal(self)-> Card:
+        # TODO: Deal one card from the deck
+        pass
+
+class EnglishDeck(Deck):
+    def __init__(self):
+        # TODO: Create a standard deck of 52 cards and shuffle them
+        pass
+
+class Hand:
+    def __init__(self):
+        # TODO: Initialize the hand
+        pass
+
+    def add_card(self, card: Card):
+        # TODO: Add a card to the hand
+        pass
+
+    def value(self)->int:
+        # TODO: Return the total value of the hand
+        pass
+
+class Player:
+    def __init__(self, name):
+        # TODO: Initialize the player's attributes
+        pass
+
+class BlackjackGame:
+    def __init__(self):
+        # TODO: Initialize the game's attributes
+        pass
+
+    def start_game(self):
+        # TODO: Start a new game and deal two cards to each player
+        pass
+
+    def hit(self)-> bool:
+        # TODO: Add a card to the player's hand
+        pass
+
+    def dealer_hit(self) -> bool:
+        # TODO: Deal cards to the dealer based on blackjack's standard rules
+        pass
+
+    def determine_winner(self):
+        # TODO: Determine and return the winner of the game
+        pass
+
+# The GUI code is provided, so students don't need to modify it
+class BlackjackGUI:
+    def __init__(self, game):
+        self.game = game
+
+        self.root = tk.Tk()
+        self.root.title("Blackjack")
+
+        # Frames for the player and the dealer
+        self.player_frame = tk.Frame(self.root)
+        self.player_frame.pack(side=tk.LEFT, padx=10)
+
+        self.deck_frame = tk.Frame(self.root)
+        self.deck_frame.pack(side=tk.LEFT, padx=10)
+
+        self.dealer_frame = tk.Frame(self.root)
+        self.dealer_frame.pack(side=tk.RIGHT, padx=10)
+
+        # "Stand" button
+        self.btn_stand = tk.Button(self.deck_frame, text="Stand", command=self.handle_stand, state=tk.NORMAL)
+        self.btn_stand.pack(side=tk.BOTTOM)
+
+        self.start_game()
+
+    def start_game(self):
+        self.game.start_game()
+        self.update_interface()
+
+    def handle_hit(self, event):
+        if self.game.hit():
+            self.update_interface()
+            self.end_game("You've busted! The house wins.")
+            return
+        self.update_interface()
+
+    def handle_stand(self):
+        self.btn_stand.config(state=tk.DISABLED)
+        while self.game.dealer_hit():
+            self.update_interface()
+        self.end_game(self.game.determine_winner())
+
+    def update_interface(self):
+        # Remove all widgets from player, deck, and dealer frames
+        for widget in self.player_frame.winfo_children():
+            widget.destroy()
+        
+        for widget in self.deck_frame.winfo_children():
+            widget.destroy()
+
+        for widget in self.dealer_frame.winfo_children():
+            widget.destroy()
+
+        # Player's cards
+        player_previous_frame = tk.Frame(self.player_frame)
+        player_previous_frame.pack(side=tk.LEFT, pady=10)
+
+        for card in self.game.player.hand.cards[:-1]:  # All cards except the last one
+            img = PhotoImage(file=card.get_image())
+            img = img.subsample(3, 3)  # Resize the image (adjust according to your preference)
+            lbl = tk.Label(player_previous_frame, image=img)
+            lbl.image = img
+            lbl.pack(side=tk.TOP, pady=5)  # Add vertical space between cards
+
+        # The last card of the player in the center
+        last_card = self.game.player.hand.cards[-1]
+        img = PhotoImage(file=last_card.get_image())
+        lbl = tk.Label(self.player_frame, image=img)
+        lbl.image = img
+        lbl.pack(side=tk.LEFT, padx=10)  # Center the last card horizontally a bit more
+        
+        # Deck in the middle
+        img = PhotoImage(file="img/card_back_01.png")
+        lbl = tk.Label(self.deck_frame, image=img, cursor="hand2")
+        lbl.image = img
+        lbl.pack(side=tk.TOP, padx=10)
+        lbl.bind("<Button-1>", self.handle_hit)
+        
+        # "Stand" button below the deck
+        self.btn_stand = tk.Button(self.deck_frame, text="Stand", command=self.handle_stand, state=tk.NORMAL)
+        self.btn_stand.pack(side=tk.BOTTOM)
+
+        # Dealer's cards
+        dealer_previous_frame = tk.Frame(self.dealer_frame)
+        dealer_previous_frame.pack(side=tk.RIGHT, pady=10)
+
+        for card in self.game.dealer.hand.cards[:-1]:  # All cards except the last one
+            img = PhotoImage(file=card.get_image())
+            img = img.subsample(3, 3)  # Resize the image (adjust according to your preference)
+            lbl = tk.Label(dealer_previous_frame, image=img)
+            lbl.image = img
+            lbl.pack(side=tk.TOP, pady=5)  # Add vertical space between cards
+
+        # The last card of the dealer in the center
+        last_card = self.game.dealer.hand.cards[-1]
+        img = PhotoImage(file=last_card.get_image())
+        lbl = tk.Label(self.dealer_frame, image=img)
+        lbl.image = img
+        lbl.pack(side=tk.RIGHT, padx=10)  # Center the last card horizontally a bit more
+
+    def end_game(self, message):
+        messagebox.showinfo("Result", message)
+        self.root.quit()
+
+    def run(self):
+        self.root.mainloop()
+if __name__ == "__main__":
+    game_logic = BlackjackGame()
+    app = BlackjackGUI(game_logic)
+    app.run()
